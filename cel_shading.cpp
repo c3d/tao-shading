bd74bb44a4f3ca944f33966fdc7c455217fef9db
Christophe de Dinechin <christophe@taodyne.com>
2012-05-21 16:30:24 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~ddd/Work/tao

--- cel_shading.cpp
+++ cel_shading.cpp
@@ -143,7 +143,7 @@ void CelShading::Draw()
 {
     if (!tested)
     {
-        licensed = tao->checkLicense("Shading 1.0", false);
+        licensed = tao->checkImpressOrLicense("Shading 1.001");
         tested = true;
     }
     if (!licensed && !tao->blink(1.0, 0.2, 300.0))
