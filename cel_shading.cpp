6e6997ebcee1b2f984ccd39c079b1c2e9718645a
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2012-08-28 09:23:10 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~jerome/Work/tao

--- cel_shading.cpp
+++ cel_shading.cpp
@@ -70,12 +70,9 @@ void CelShading::Draw()
 {
     if (!tested)
     {
-        licensed = tao->checkImpressOrLicense("Shading 1.003");
+        licensed = tao->checkImpressOrLicense("Shading 1.004");
         tested = true;
     }
-
-    if (!licensed && !tao->blink(1.0, 1.0, 300.0))
-        return;
 
     checkGLContext();
 
