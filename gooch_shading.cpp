e49a4bab2ab92fcb310ea6c11fcaa16100bb7f25
Jerome Forissier <jerome@taodyne.com>
2013-05-03 12:26:31 +0200

    Remove license checks for modules that were calling checkImpressOrLicense(). Closes #2989.

--- gooch_shading.cpp
+++ gooch_shading.cpp
@@ -108,12 +108,6 @@ void GoochShading::Draw()
 //   Apply gooch shading
 // ----------------------------------------------------------------------------
 {
-    if (!tested)
-    {
-        licensed = tao->checkImpressOrLicense("Shading 1.005");
-        tested = true;
-    }
-
     checkGLContext();
 
     uint prg_id = 0;
