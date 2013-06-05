fa0f51a8d2b0f35bcd9df4dd10e0b6ebf89ece34
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2013-06-05 18:12:24 +0200

    Remove some unneeded functions from module api. Refs #3044.

--- cel_shading.cpp
+++ cel_shading.cpp
@@ -95,7 +95,7 @@ void CelShading::Draw()
 
         if(tao->isGLExtensionAvailable("GL_EXT_gpu_shader4"))
         {
-            GLint lightsmask = tao->EnabledLights();
+            GLint lightsmask =  GL.LightsMask();
             GL.Uniform(uniforms["lights"], lightsmask);
         }
     }
--- gooch_shading.cpp
+++ gooch_shading.cpp
@@ -141,7 +141,7 @@ void GoochShading::Draw()
 
         if(tao->isGLExtensionAvailable("GL_EXT_gpu_shader4"))
         {
-            GLint lightsmask = tao->EnabledLights();
+            GLint lightsmask =  GL.LightsMask();
             GL.Uniform(uniforms["lights"], lightsmask);
         }
     }
